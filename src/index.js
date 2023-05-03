eb32a3e92ee1a5007a164a1973652b5fa8db9e49
Oneloz <qivanios53@gmail.com>
2023-05-04 00:36:20 +0300

    added some packages

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const searchInput = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
